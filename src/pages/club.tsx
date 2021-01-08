daf3d158957925d1aefa8f43670d865ebfdd0574
Philipp Riegger <philipp.riegger@reservix.de>
2021-01-08 13:00:46 +0100

    improve club page

--- src/pages/club.tsx
+++ src/pages/club.tsx
@@ -1,52 +1,73 @@
 import React from 'react';
 import { useI18next } from 'gatsby-plugin-react-i18next';
-import { Paper, Typography } from '@material-ui/core';
-import {
-  Timeline,
-  TimelineItem, 
-  TimelineSeparator,
-  TimelineConnector, 
-  TimelineContent,
-  TimelineOppositeContent,
-  TimelineDot
-} from '@material-ui/lab';
+import { Accordion, AccordionSummary, AccordionDetails, Box, Typography } from '@material-ui/core';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import Layout from '../components/layout';
-import { isMobile } from '../utils/helper';
 import '../styles/club.scss';
+
+interface IHistoryItem {
+  startYear: number,
+  endYear: number | null,
+  years: Array<number>
+}
 
 export default function ClubPage() {
   const { t } = useI18next();
 
-  const timeLineItems = [
-    {year: 1980, title: "Gründung", text: "t.b.d"},
-    {year: 1983, title: "Zweites Ereignis", text: "t.b.d"},
+  const historyItems: Array<IHistoryItem> = [
+    {
+      "startYear": 1980,
+      "endYear": 1990,
+      "years": [1980, 1981, 1983, 1984, 1986, 1987, 1988, 1989, 1990]
+    },
+    {
+      "startYear": 1991,
+      "endYear": 1999,
+      "years": [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
+    },
+    {
+      "startYear": 2001,
+      "endYear": 2010,
+      "years": [2001, 2003, 2004, 2005, 2008, 2009, 2010]
+    },
+    {
+      "startYear": 2012,
+      "endYear": null,
+      "years": [2012, 2013, 2016, 2018, 2019]
+    }
   ];
 
   return (
     <Layout>
-      <Timeline align={isMobile() ? 'left' : 'alternate'}>
-        {timeLineItems.map((timeLineItem, index) => (
-          <TimelineItem key={index}>
-            <TimelineOppositeContent className={isMobile() ? 'timeline-year' : ''}>
-              <Typography variant="body2" color="textSecondary">
-                {timeLineItem.year}
+      <Typography variant="h2">{t("history.title")}</Typography>
+      <div className="utils-margin-bottom-10"></div>
+      <div className="history-image-container">
+        <img src="/img/legends.jpg"/>
+      </div>
+      <div className="utils-margin-bottom-30"></div>
+      <Typography>{t("history.intro")}</Typography>
+      <div className="utils-margin-bottom-30"></div>
+      <div className="accordion">
+        {historyItems.map((historyItem, index) => (
+          <Accordion square key={index}>
+            <AccordionSummary className="accordion-summary" expandIcon={<ExpandMoreIcon/>}>
+              <Typography variant="h6">
+                {historyItem.startYear} {t("until")} {historyItem.endYear === null ? t("today") : historyItem.endYear}
               </Typography>
-            </TimelineOppositeContent>
-            <TimelineSeparator>
-              <TimelineDot/>
-              <TimelineConnector/>
-            </TimelineSeparator>
-            <TimelineContent>
-              <Paper elevation={3} className="paper">
-                <Typography variant="h6" component="h1">
-                  {t(timeLineItem.title)}
-                </Typography>
-                <Typography>{t(timeLineItem.text)}</Typography>
-              </Paper>
-            </TimelineContent>
-          </TimelineItem>
+            </AccordionSummary>
+            <AccordionDetails>
+              <div>
+                {historyItem.years.map((year) => (
+                  <div key={year} className="utils-margin-bottom-20">
+                    <Box fontWeight="fontWeightBold">{year}</Box>
+                    <div dangerouslySetInnerHTML={{__html: t(`history.${year}`, {interpolation: {escapeValue: false}})}}/>
+                  </div>
+                ))}
+              </div>
+            </AccordionDetails>
+          </Accordion>
         ))}
-      </Timeline>
+      </div>
     </Layout>
   );
 }
